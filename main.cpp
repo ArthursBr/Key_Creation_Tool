d7173aba3776bc53139aff27aa3e480f6b3621d1
Arthur <arthur.soares.barbosa.leal7@gmail.com>
2025-02-25 20:16:45 -0300

    Incrementação para receber a chave.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-
+    string chavePrimaria;
+    cout << "Informe a chave: " << endl;
+    cin >> chavePrimaria;
     return 0;
 }
