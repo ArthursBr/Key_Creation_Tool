3a621077e6068f4de73ea583e02dd9f859879624
Arthur <arthur.soares.barbosa.leal7@gmail.com>
2025-02-26 21:42:36 -0300

    Código implementado.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,29 @@ using namespace std;
 
 int main()
 {
-    string chavePrimaria;
-    cout << "Informe a chave: " << endl;
-    cin >> chavePrimaria;
+    string textoPrimario, textoCifrado = "";
+    cout << "Informe o texto: " << endl;
+    getline(cin, textoPrimario);
+
+    int chave;
+    cout << "Defina a chave: " << endl;
+    cin >> chave;
+
+    for(char c : textoPrimario)
+    {
+        if(isupper(c)){
+            textoCifrado += char(int(c + chave - 'A')  % 26 + 'A');
+        }
+        else if (islower(c)) 
+        {
+            textoCifrado += char(int(c + chave - 'a') % 26 + 'a');
+        }
+        else 
+        {
+            textoCifrado += c;
+        }
+    }
+    cout << "Texto Cifrado: \n" << textoCifrado << endl;
+
     return 0;
 }
